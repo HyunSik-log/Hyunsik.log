3ae1785243bb3e751d4a2c53dd7d06bd0f79815b
HyunSik <49386467+HyunSik-log@users.noreply.github.com>
2022-04-18 00:39:07 +0900

    TD

---
+++ BIT/Tel Dir/Tel/src/tel/lib/TelLib.java
@@ -0,0 +1,30 @@
+package tel.lib;
+
+import java.util.Scanner;
+
+public class TelLib {
+	private static Scanner scan = new Scanner(System.in);
+	
+	public static int InputInteger(String msg) {
+		System.out.print(msg + " : ");
+		return Integer.parseInt(scan.nextLine());
+	}
+	
+	public static float InputFloat(String msg) {
+		System.out.print(msg + " : ");
+		return Float.parseFloat(scan.nextLine());
+	}
+	
+	public static char InputChar(String msg) {
+		System.out.print(msg + " : ");
+		return scan.nextLine().charAt(0);
+	}
+	
+	public static String InputString(String msg) {
+		System.out.print(msg + " : ");
+		return scan.nextLine();
+	}
+}
+
+
+
